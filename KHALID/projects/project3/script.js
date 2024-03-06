f12e889f331a29b7a69565ac5878a0d0c9861d63
khalid marzoug <KHALIDMARZOUG9@gmail.com>
2024-03-06 15:25:44 +0100

    test api

--- KHALID/projects/project3/script.js
+++ KHALID/projects/project3/script.js
@@ -1,24 +1,6 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   e.preventDefault();
-  let container = document.createElement("div");
-  container.classList.add("bg-info-subtle");
-  container.classList.add("w-75");
-  container.classList.add("h-75");
-  container.classList.add("d-flex");
-  container.classList.add("justify-content-center");
-  container.classList.add("gap-1");
-  container.classList.add("p-1");
-  document.body.append(container);
-  let div = document.createElement("div");
-  div.style.width = "10px";
-  div.style.height = "50%";
-  div.classList.add("bg-warning");
-  container.append(div);
-  let containerRect = container.getBoundingClientRect();
-  let divRect = div.getBoundingClientRect();
-  let numberDivInside = containerRect.width / divRect.width;
-  numberDivInside = Math.floor(numberDivInside);
-  let listDivs = new Array(numberDivInside);
+  // list of bg of bootstrap for random bg:
   const bgColors = [
     "bg-primary",
     "bg-secondary",
@@ -29,30 +11,60 @@ document.addEventListener("DOMContentLoaded", (e) => {
     "bg-dark",
     "bg-white",
   ];
-  const heights = [
-    "10%",
-    "20%",
-    "30%",
-    "40%",
-    "50%",
-    "60%",
-    "70%",
-    "80%",
-    "90%",
-  ];
-
+  // function gives random index to a list :
   function randomIndex(len) {
     let randomIndex = Math.floor(Math.random() * len);
     return randomIndex;
   }
+  // create container and style it :
+  let container = document.createElement("div");
+  container.classList.add("bg-info-subtle");
+  container.classList.add("w-75");
+  container.classList.add("h-75");
+  container.classList.add("d-flex");
+  container.classList.add("justify-content-center");
+  container.classList.add("gap-1");
+  container.classList.add("p-1");
+  document.body.append(container);
+  // create the reference of the divs and style it:
+  let div = document.createElement("div");
+  div.style.width = "10px";
+  div.classList.add("border");
+  div.style.height = Math.floor(Math.random() * 100) + "%";
+  div.classList.add(bgColors[randomIndex(bgColors.length)]);
+  container.append(div);
+  // get the sizes of the container and the div :
+  let containerRect = container.getBoundingClientRect();
+  let divRect = div.getBoundingClientRect();
+  // get the number should be inside the container :
+  let numberDivInside = containerRect.width / divRect.width;
+  numberDivInside = Math.floor(numberDivInside);
+  // create the list of divs should be , but with empty places :
+  let listDivs = new Array(numberDivInside);
+  // create the other divs with a random height and bg :
   for (let i = 0; i < numberDivInside; i++) {
     listDivs[i] = document.createElement("div");
     listDivs[i].style.width = divRect.width + "px";
-    listDivs[i].style.height = heights[randomIndex(heights.length)];
-    listDivs[i].classList = "border";
+    listDivs[i].style.height = Math.floor(Math.random() * 100) + "%";
+    listDivs[i].classList.add("border");
     listDivs[i].classList.add(bgColors[randomIndex(bgColors.length)]);
     setTimeout(() => {
       container.append(listDivs[i]);
-    }, 100 * i);
+    }, 50 * i);
   }
+  let button1 = document.createElement("a");
+  button1.setAttribute("href", "#");
+  button1.textContent = "Asc";
+  button1.classList.add("btn");
+  button1.classList.add("btn-success");
+  document.body.append(button1);
+  let button2 = document.createElement("a");
+  button2.setAttribute("href", "#");
+  button2.textContent = "Desc";
+  button2.classList.add("btn");
+  button2.classList.add("btn-warning");
+  document.body.append(button2);
 });
+
+let buttons = document.getElementsByTagName("button");
+console.log(buttons);
---
+++ apiTestJs.js
@@ -0,0 +1,17 @@
+const userName = "MMSO-DD105";
+const reposName = "MMSO_Projects";
+const listOwners = ["KHALID", "ANAS", "ZAKARIA", "MOHAMED"];
+const apiUrl = `https://api.github.com/repos/${userName}/${reposName}/contents/${listOwners[0]}/projects`;
+
+let promise = fetch(apiUrl); // fetch function return a promise
+
+promise
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log("There was a problem with your fetch operation ---> ", error);
+  });
