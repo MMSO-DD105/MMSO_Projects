5d1c46af5313c80643c585b2e719b114c4b4f4ab
Anasmahfoud <156627676+Anasmahfoud@users.noreply.github.com>
2024-03-20 17:07:54 +0000

    fixed links 2

--- ANAS/script.js
+++ ANAS/script.js
@@ -8,7 +8,7 @@ async function Getdata(){
   const dataJSON = await data.json()
   dataJSON.forEach((itemValue,index)=>{
     if (itemValue.type == "dir"){
-    main.innerHTML += "<div class='card python text-center col-lg-custom col-12  col-md-6'style='height:70%; '><img src=' "+ "ProjectImages/" + itemValue.name+ ".jpg" +"'  class='card-img-top h-50 "+ "idimg" + index +"'  /><div class='card-body'><h6 class='card-title'>"+ itemValue.name +"</h6><p class='card-text " + `iddes${index}`+ "'>Some quick example text to build on the card title and make up the bulk of the card's content.</p><a href='/projects/" + itemValue.name +  "' class='btn btn-primary w-75 h-25 fs-6'>Lanche</a></div></div>"
+    main.innerHTML += "<div class='card python text-center col-lg-custom col-12  col-md-6'style='height:70%; '><img src=' "+ "ProjectImages/" + itemValue.name+ ".jpg" +"'  class='card-img-top h-50 "+ "idimg" + index +"'  /><div class='card-body'><h6 class='card-title'>"+ itemValue.name +"</h6><p class='card-text " + `iddes${index}`+ "'>Some quick example text to build on the card title and make up the bulk of the card's content.</p><a href='projects/" + itemValue.name +  "' class='btn btn-primary w-75 h-25 fs-6'>Lanche</a></div></div>"
     }
     })
 
